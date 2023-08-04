cba79e976dd2c7ebfa8faf61d2a10954bae687de
Livingstone Maghanga <stoneriga@gmail.com>
2023-08-04 15:49:05 +0300

    Firebase Integration

---
+++ app/Shared/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2kGp07HgS_V9waamQCGjozXxzc6nHmTo",
+  authDomain: "novamessenger-39c8e.firebaseapp.com",
+  databaseURL: "https://novamessenger-39c8e-default-rtdb.firebaseio.com",
+  projectId: "novamessenger-39c8e",
+  storageBucket: "novamessenger-39c8e.appspot.com",
+  messagingSenderId: "248000963636",
+  appId: "1:248000963636:web:531557702be83778f1d661"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
