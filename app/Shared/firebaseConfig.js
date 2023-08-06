629e916f279eda80c5308abf7ceb93ac2ae34940
Livingstone Maghanga <stoneriga@gmail.com>
2023-08-06 16:52:37 +0300

    Firebase config update and database fetch

--- app/Shared/firebaseConfig.js
+++ app/Shared/firebaseConfig.js
@@ -1,20 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB2kGp07HgS_V9waamQCGjozXxzc6nHmTo",
-  authDomain: "novamessenger-39c8e.firebaseapp.com",
-  databaseURL: "https://novamessenger-39c8e-default-rtdb.firebaseio.com",
-  projectId: "novamessenger-39c8e",
-  storageBucket: "novamessenger-39c8e.appspot.com",
-  messagingSenderId: "248000963636",
-  appId: "1:248000963636:web:531557702be83778f1d661"
+  apiKey: "AIzaSyBE_ZnDmr0HnlmEWjUTZquc9HBwwa79IK0",
+  authDomain: "stone-b9694.firebaseapp.com",
+  projectId: "stone-b9694",
+  storageBucket: "stone-b9694.appspot.com",
+  messagingSenderId: "454228889501",
+  appId: "1:454228889501:web:66bcf1baf76da94e96a974",
+  measurementId: "G-0QZQDRTRQ4"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-export default app;
+export default app
--- app/[userId]/page.jsx
+++ app/[userId]/page.jsx
@@ -1,13 +1,30 @@
-"use client"
-import React, { useEffect } from 'react'
+"use client";
+import { getFirestore } from "firebase/firestore";
+import React, { useEffect } from "react";
+import { doc, getDoc } from "firebase/firestore";
+import app from "../Shared/firebaseConfig";
 
-function Profile({params}) {
-    useEffect(()=>{
-        console.log(params.userId.replace('%40', '@'))
-    },[params])
-  return (
-    <div>Profile</div>
-  )
+function Profile({ params }) {
+  const db = getFirestore(app);
+  useEffect(() => {
+    console.log(params.userId.replace("%40", "@"));
+    if(params){
+      getUserInfo(params.userId.replace("%40", "@"))
+    }
+  }, [params]);
+
+  const getUserInfo = async (email) => {
+    const docRef = doc(db, "user", email);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+    } else {
+      //docSnap.data() will be undefined in this case
+      console.log("No such document!");
+    }
+  };
+  return <div>Profile</div>;
 }
 
-export default Profile
+export default Profile;
