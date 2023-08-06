4ef20dee0e2b3bfd07a6d9161df63e02a0185db7
Livingstone Maghanga <stoneriga@gmail.com>
2023-08-06 10:05:01 +0300

    login session update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,13 @@
+"use client"
 import Image from "next/image";
 import React from "react";
 import { HiSearch, HiBell, HiChat } from "react-icons/hi"
+import { useSession, signIn, signOut } from "next-auth/react";
 
 function Header() {
+  const {data: session} = useSession()
+
+  console.log(session)
   return (
     <div className="flex gap-3 md:gap-2 items-center p-6">
       <Image 
@@ -21,13 +26,15 @@ function Header() {
         <HiSearch className="text-[25px] text-gray-500 md:hidden" />
         <HiBell className="text-[40px] text-gray-500" />
         <HiChat className="text-[40px] text-gray-500"  />
+        {session?.user?
         <Image 
-        src="/profile.jpg"
+        src={session?.user?.image}
         alt="profile image"
         width={50}
         height={50}
         className="hover:bg-gray-600 p-2 rounded-full cursor-pointer" 
-        />
+        />:
+        <button className="font-semibold rounded-full px-4" onClick={()=>signIn()}>Login</button>}
     </div>
   );
 }
